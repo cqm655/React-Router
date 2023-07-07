9eecb22de8daa678ad883a779d8e822777ab276e
iurie.balan <iurie.balan@112.md>
2023-07-07 09:30:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <Products /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function HomePage() {
+  //   const navigate = useNavigate();
+
+  //   function navigationHandler() {
+  //     navigate("/products");
+  //   }
+  return (
+    <>
+      <h1>Main PAge!</h1>
+      <Link to={"/products"}>go to products</Link>
+      {/* <button onClick={navigationHandler}>Navigate to Products</button> */}
+    </>
+  );
+}
+
+export default HomePage;
